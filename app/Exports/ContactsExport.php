3f3f324bb350f797b998e4598adbc999d4595aa0
Chetan Singh <chetan.singh@webeesocial.com>
2024-08-28 18:22:36 +0530

    fixed validation error

--- app/Exports/ContactsExport.php
+++ app/Exports/ContactsExport.php
@@ -24,17 +24,29 @@ class ContactsExport implements FromCollection , WithHeadings , WithMapping
             'Email',
             'Phone',
             'Speciality',
+            'Message',
+            'Lead Type',
+            'File',
             'Created At',
         ];
     }
 
     public function map($contact): array
     {
+        $file_path = '';
+
+        if($contact->file){
+            $file_path = env('APP_URL').'/storage/'.$contact->file;
+        }
+
         return [
             $contact->name,
             $contact->email,
             $contact->phone,
             $contact->specialty,
+            $contact->message,
+            $contact->lead_type,
+            $file_path,
             $contact->created_at,
         ];
     }
