d94a73e8a96aa4e67dd460bc066f630b418c5f0e
ajwebee1326 <ajay.kumar@webeesocial.com>
2024-08-29 18:48:29 +0530

    worked on admin panel contact page route

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
 
   <div class="app-brand demo ">
-    <a href="{{route('admin.dashboard')}}" class="app-brand-link">
+    <a href="{{route('admin.contact')}}" class="app-brand-link">
       <span class="app-brand-text demo menu-text fw-bold ms-2"> <img src="{{ asset('frontend') }}/images/Logo.webp" width="180" alt="Logo" height="40"></span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
@@ -15,7 +15,7 @@
      <!-- Apps & Pages -->
     
     <li class="menu-item active">
-      <a href="{{route('admin.dashboard') }}" class="menu-link">
+      <a href="{{route('admin.contact') }}" class="menu-link">
         <i class="menu-icon tf-icons bx bx-message-rounded-dots"></i>
         <div class="text-truncate" data-i18n="Inquiry">Inquiry</div>
       </a>
