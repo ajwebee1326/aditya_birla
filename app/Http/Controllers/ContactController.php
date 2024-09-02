bbf6b8347d35feefca59a1d5b7133337610cf0e0
ajwebee1326 <ajay.kumar@webeesocial.com>
2024-09-02 14:45:55 +0530

    worked on leads order

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -15,7 +15,7 @@ class ContactController extends Controller
      */
     public function index()
     {
-        $contacts = Contact::orderBy('created_at', 'desc')->get();  
+        $contacts = Contact::orderBy('created_at', 'desc')->get();
         return view('admin.contact.index', compact('contacts')); 
     }
 
@@ -24,7 +24,7 @@ class ContactController extends Controller
      */
     public function create()
     {
-       
+
     }
 
     /**
